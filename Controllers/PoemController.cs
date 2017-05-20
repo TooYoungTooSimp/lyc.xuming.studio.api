6c31083a4494971879676bfa71b0a04d646e192e
TooYoungTooSimp <6648049+TooYoungTooSimp@users.noreply.github.com>
2017-05-20 18:01:22 +0800

    2017年 05月 20日 星期六 18:01:22 CST

---
+++ Controllers/PoemController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace lyc.xuming.studio.api.Controllers
+{
+    [Route("api/[controller]")]
+    public class PoemController : Controller
+    {
+        [HttpGet]
+        public string Get() => HaPoem.GetString();
+        [HttpGet("{id}")]
+        public string Get(int id) => HaPoem.GetString(id);
+    }
+    static class HaPoem
+    {
+        static private string[] strs = File.ReadAllLines("poem.txt");
+        static private Random rnd = new Random();
+        static public string GetString() => strs[rnd.Next(0, strs.Length)];
+        static public string GetString(int id) => strs[id % strs.Length];
+    }
+}
