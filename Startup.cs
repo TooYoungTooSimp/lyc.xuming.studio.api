adc164dbafcab786960d57ff94c5fcec54c7f38d
TooYoungTooSimp <6648049+TooYoungTooSimp@users.noreply.github.com>
2017-08-18 00:00:25 +0800

    Fri, 18 Aug 2017 00:00:25 +0800

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -27,8 +27,13 @@ namespace lyc.xuming.studio.api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseMvc();
         }
     }
