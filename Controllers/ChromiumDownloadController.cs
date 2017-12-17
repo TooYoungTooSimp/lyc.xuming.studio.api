4a06084fedf0298afd21422dbda632c9d16b82a2
TooYoungTooSimp <6648049+TooYoungTooSimp@users.noreply.github.com>
2017-12-17 20:02:39 +0800

    Add Chromium Download Urls

---
+++ Controllers/ChromiumDownloadController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net;
+
+namespace lyc.xuming.studio.api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/ChromiumDownload")]
+    public class ChromiumDownloadController : Controller
+    {
+        static Dictionary<string, KeyValuePair<string, DateTime>> ChromiumBuilds = new Dictionary<string, KeyValuePair<string, DateTime>>();
+        static string templatePrefix = "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/";
+        static string buildNumTemplate = templatePrefix + "{0}%2FLAST_CHANGE?alt=media";
+        static Dictionary<string, string> downloadUrlTemplates = new Dictionary<string, string>();
+        public ChromiumDownloadController()
+        {
+            if (ChromiumBuilds.Count == 0)
+                foreach (var platformStr in new string[] { "Win", "Win_x64", "Linux", "Linux_x64", "Mac" })
+                    ChromiumBuilds[platformStr] = new KeyValuePair<string, DateTime>("", new DateTime(0));
+            if (downloadUrlTemplates.Count == 0)
+            {
+                downloadUrlTemplates.Add("Win", templatePrefix + "Win%2F{0}%2Fchrome-win32.zip?alt=media");
+                downloadUrlTemplates.Add("Win_x64", templatePrefix + "Win_x64%2F{0}%2Fchrome-win32.zip?alt=media");
+                downloadUrlTemplates.Add("Linux", templatePrefix + "Linux %2F{0}%2Fchrome-linux.zip?alt=media");
+                downloadUrlTemplates.Add("Linux_x64", templatePrefix + "Linux_x64 %2F{0}%2Fchrome-linux.zip?alt=media");
+                downloadUrlTemplates.Add("Mac", templatePrefix + "Mac%2F{0}%2Fchrome-mac.zip?alt=media");
+            }
+        }
+        // GET: api/ChromiumDownload
+        [HttpGet]
+        public object Get()
+        {
+            var wc = new WebClient();
+            var now = DateTime.Now;
+            foreach (var platform in ChromiumBuilds.Keys.ToArray())
+                if (now - ChromiumBuilds[platform].Value > TimeSpan.FromMinutes(30))
+                    ChromiumBuilds[platform] = new KeyValuePair<string, DateTime>(wc.DownloadString(String.Format(buildNumTemplate, platform)), now);
+            return ChromiumBuilds.Keys.ToDictionary(platform => platform, platform => String.Format(downloadUrlTemplates[platform], ChromiumBuilds[platform].Key));
+        }
+
+        // GET: api/ChromiumDownload/{platform}
+        [HttpGet("{platform}", Name = "Get")]
+        public string Get(string platform)
+        {
+            if (!ChromiumBuilds.ContainsKey(platform)) return "";
+            else
+            {
+                var wc = new WebClient();
+                var now = DateTime.Now;
+                if (now - ChromiumBuilds[platform].Value > TimeSpan.FromMinutes(30))
+                    ChromiumBuilds[platform] = new KeyValuePair<string, DateTime>(wc.DownloadString(String.Format(buildNumTemplate, platform)), now);
+                return String.Format(downloadUrlTemplates[platform], ChromiumBuilds[platform].Key);
+            }
+        }
+    }
+}
