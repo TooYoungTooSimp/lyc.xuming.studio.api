045633b8ef895c38fb12768e8be8313ec0f66e0b
TooYoungTooSimp <6648049+TooYoungTooSimp@users.noreply.github.com>
2021-08-14 16:27:44 +0800

    Sat, 14 Aug 2021 16:27:44 GMT

---
+++ Controllers/PixivController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace lyc.xuming.studio.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PixivController : ControllerBase
+    {
+        private static readonly HttpClient HttpClient = new();
+        [Route("[action]")]
+        public async Task<string> IndexAsync()
+        {
+            return await HttpClient.GetStringAsync("https://www.pixiv.net/");
+        }
+    }
+}
