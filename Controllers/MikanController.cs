994fae2e32bc79d65f2be137379b8ea4d84061e8
TooYoungTooSimp <6648049+TooYoungTooSimp@users.noreply.github.com>
2023-05-27 14:21:27 +0800

    Sat, 27 May 2023 14:21:27 GMT

---
+++ Controllers/MikanController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lyc.xuming.studio.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MikanController : ControllerBase
+    {
+        private static readonly HttpClient httpClient = new();
+        private static readonly Regex linkMatcher = new("https://mikanani.me/Download/(.*?)/(.*?).torrent");
+        [Route("rss/{token}")]
+        public async Task<IResult> RSSAsync(string token)
+        {
+            var res = await httpClient.GetStringAsync($"https://mikanani.me/RSS/MyBangumi?token={token}");
+            var magnet = linkMatcher.Replace(res, "magnet:?xt=urn:btih:$2");
+            return Results.Text(magnet, "application/xml");
+        }
+    }
+}
